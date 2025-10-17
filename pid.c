825d084befa518955766113f3cf31a5300ea3167
twiggle <68298680+the-wiggler@users.noreply.github.com>
2025-10-16 23:09:42 -0400

    changed to quaternions

--- pid.c
+++ pid.c
@@ -6,50 +6,51 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // PID tuning coefficients
-PID_t roll_attitude_pid_coeffs  = { 50, 5, 17 };
-PID_t pitch_attitude_pid_coeffs = { 50, 5, 17 };
-PID_t yaw_attitude_pid_coeffs   = { 10, 2, 5 };
+PID_t PID_COEFFS = {50, 50, 15};
 
 // PID error state storage
-// this stores the PID error values that we use to know "how much" we should correct the motors
-PID_errors_t roll_attitude_errors   = { 0, 0, 0, 0 };
-PID_errors_t pitch_attitude_errors  = { 0, 0, 0, 0 };
-PID_errors_t yaw_attitude_errors    = { 0, 0, 0, 0 };
+
 
 // function that calculates PID output based on comparing the setpoint vs the current state
-float calculate_pid(const PID_t *pid_coeffs, PID_errors_t *errors, float setpoint, 
-                    float current_state) {
-    // get the current time
-    float current_time = get_time(); // this function doesnt exist
-
-    // calculate the change in time
-    float delta_time = (current_time - errors->previous_time);
-
-    // calculates the current absolute error from the setpoint
-    // proportional term
-    errors->current_error = setpoint - current_state;
-
-    // calculate integral term
-    errors->integral += errors->current_error * delta_time;
-
-    // calculate derivative term
-    float derivative = 0;
-    if (delta_time > 0) {
-        derivative = (errors->current_error - errors->previous_error) / delta_time;
-    }
-
-    // calculate PID output using the formula (look on google for "pid formula")
-    float output = (pid_coeffs->kp * errors->current_error) +
-                      (pid_coeffs->ki * errors->integral) +
-                      (pid_coeffs->kd * derivative);
+vector_t calculate_pid(PID_t pid_coeffs, PID_errors_t *errors, vector_t setpoint,
+                       vector_t current_state) {
+    vector_t outputs;
     
-    // update for the next iteration
-    errors->previous_error = errors->current_error;
+    float current_time = get_time();
+    float delta_time = current_time - errors->previous_time;
+    float inv_delta_time = 1.0f / delta_time;
+    
+    // process all axes
+    float *errs[3] = {&errors->current_error.x, &errors->current_error.y, &errors->current_error.z};
+    float *errs_prev[3] = {&errors->previous_error.x, &errors->previous_error.y, &errors->previous_error.z};
+    float *integral[3] = {&errors->integral.x, &errors->integral.y, &errors->integral.z};
+    float *proportional[3] = {&setpoint.x, &setpoint.y, &setpoint.z};
+    float *cs[3] = {&current_state.x, &current_state.y, &current_state.z};
+    float *out[3] = {&outputs.x, &outputs.y, &outputs.z};
+    
+    for(int i = 0; i < 3; i++) {
+        // calculate error
+        *errs[i] = *proportional[i] - *cs[i];
+        
+        // update integral
+        *integral[i] += *errs[i] * delta_time;
+        
+        // calculate derivative
+        float derivative = (*errs[i] - *errs[i]) * inv_delta_time;
+        
+        // calculate PID output
+        *out[i] = pid_coeffs.kp * (*errs[i]) + 
+                  pid_coeffs.ki * (*integral[i]) + 
+                  pid_coeffs.kd * derivative;
+        
+        // update previous error
+        *errs_prev[i] = *errs[i];
+    }
+    
     errors->previous_time = current_time;
-
-    // returns final PID output value that was calculated above
-    return output;
+    return outputs;
 }
+
 
 int16_t get_time() {
     return 100;
--- pid.h
+++ pid.h
@@ -6,8 +6,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// note: ive just made all of the PID types int16_t type since im not sure what processing power
-// we'll have yet.
+// 3 dimensional vector type
+typedef vector_t {
+    float x;
+    float y;
+    float z;
+};
 
 // PID Coefficients
 typedef struct {
@@ -18,32 +22,26 @@ typedef struct {
 
 // PID error tracking values
 typedef struct {
-    float current_error;
-    float previous_error;
-    float integral;
-    // previous_time variable is to track the time taken since the last PID calculation was
-    // so that the d and i parts can work as they should. This isnt necessary if we use
-    // an interrupt that has an exact time (then we can use a constant)
+    vector_t current_error;
+    vector_t previous_error;
+    vector_t integral;
     float previous_time;
 } PID_errors_t;
 
-// roll acceleration rate pid controller 
-extern PID_t roll_rate_pid;
-extern PID_t pitch_rate_pid;
-extern PID_t yaw_rate_pid;
+typedef struct {
+    float w; // scalar part of the quaternion
+    float x, y, z; // vector parts of the quaternion
+} Quaternion_vector_t;
+
+// attitude change rate pid controller 
+extern PID_t attitude_pid;
 
 // roll acceleration rate pid error values 
-extern PID_errors_t roll_rate_errors;
-extern PID_errors_t pitch_rate_errors;
-extern PID_errors_t yaw_rate_errors;
+extern PID_errors_t attitude_errors;
 
-// max allowed rate
-extern float max_roll_rate;
-extern float max_pitch_rate;
-extern float max_yaw_rate;
 
-float calculate_pid(const PID_t *pid_coeffs, PID_errors_t *errors, float setpoint,
-                    float current_state);
+vector_t calculate_pid(PID_t pid_coeffs, PID_errors_t *errors, vector_t setpoint, 
+                       vector_t current_state);
 void update_motors_from_pid(float roll_output, float pitch_output, float yaw_output,
                             float throttle);
 
